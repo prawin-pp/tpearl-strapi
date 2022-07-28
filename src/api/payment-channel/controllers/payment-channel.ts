9d1f9916e88f98787b65cec2c5a20a45bb84f940
Prawin Rungpiriyadej <parwin.pp@gmail.com>
2022-07-28 22:43:37 +0700

    feat: payment-channels collection

---
+++ src/api/payment-channel/controllers/payment-channel.ts
@@ -0,0 +1,7 @@
+/**
+ *  payment-channel controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::payment-channel.payment-channel');
---
+++ src/api/payment-channel/services/payment-channel.ts
@@ -0,0 +1,7 @@
+/**
+ * payment-channel service.
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::payment-channel.payment-channel');
