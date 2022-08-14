d98b9d702356b3d91e8cb9ee0c8c500cd7787139
Prawin Rungpiriyadej <parwin.pp@gmail.com>
2022-08-14 21:02:21 +0700

    updated

--- devtools/seed/8_product_addons.sql
+++ devtools/seed/8_product_addons.sql
@@ -12,8 +12,12 @@ VALUES
   (1, 'ไข่มุก', NOW(), NOW(), NOW()),
   (2, 'เยลลี่ฟรุตสลัด', NOW(), NOW(), NOW()),
   (3, 'บุกบราวน์ชูการ์', NOW(), NOW(), NOW()),
-  (4, 'ปั่น', NOW(), NOW(), NOW());
+  (4, 'ฮันนี่เจลลี่', NOW(), NOW(), NOW()),
+  (5, 'ปั่น', NOW(), NOW(), NOW()) ON CONFLICT (id) DO
+UPDATE
+SET
+  "name" = EXCLUDED."name";
 
-ALTER SEQUENCE product_addons_id_seq RESTART 5;
+ALTER SEQUENCE product_addons_id_seq RESTART 6;
 
 END TRANSACTION;
